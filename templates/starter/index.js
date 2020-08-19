08f7e26270e3e5ea7d75c1b36baf28662a6f2804
I. Dan Calinescu <idancalinescu@gmail.com>
2020-08-20 02:35:03 +0300

    initial template

---
+++ templates/starter/index.js
@@ -0,0 +1,27 @@
+class Template {
+    get stack () {
+        return "jayesse"
+    }
+
+    get packer () {
+        return "papanache"
+    }
+    
+    get archiveFiles() {
+        return [
+            "carmel/chunks/intro/**/*",
+            "carmel/assets/en/images/logo.png", 
+            "carmel/assets/en/images/logo-light.png", 
+            "tsconfig.json"
+        ]
+    }
+
+    get files() {
+        return  [
+          ".carmel.json",
+          "carmel/assets/**/*" 
+        ]
+    }
+}
+
+module.exports = Template
