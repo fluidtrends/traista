64a9332c110d27b836a121c222a057fd51084fc9
I. Dan Calinescu <idancalinescu@gmail.com>
2020-12-27 23:33:47 +0200

    added more data

--- templates/starter/index.js
+++ templates/starter/index.js
@@ -12,6 +12,7 @@ class Template {
             "carmel/chunks/intro/**/*",
             "carmel/assets/en/images/logo.png", 
             "carmel/assets/en/images/logo-light.png", 
+            "data/**/*",
             "tsconfig.json"
         ]
     }
@@ -19,7 +20,8 @@ class Template {
     get files() {
         return  [
           ".carmel.json",
-          "carmel/assets/**/*" 
+          "carmel/assets/**/*",
+          "data/**/*" 
         ]
     }
 }
